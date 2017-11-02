4614fbe0ec91ba22b5f72b6fa59c681ea83cd26d
Robert Rosebury <r.rosebury@pitchero.com>
2017-11-02 10:49:56 +0000

    WEB-2030: Added in the Customer resource and unit test.

---
+++ src/Customer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ResellerClub;
+
+class Customer
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    public function __construct(int $customer_id)
+    {
+        $this->id = $customer_id;
+    }
+
+    public function id(): int
+    {
+        return (int) $this->id;
+    }
+}
