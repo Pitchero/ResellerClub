1a42ffa473f0cd8608bb42436721ac725c89a34e
Robert Rosebury <r.rosebury@pitchero.com>
2017-11-03 17:42:23 +0000

    WEB-2035: Test the response of the BusinessEmailOrder class create method returns correct response, currenlty doesn't pass.

--- tests/unit/Orders/BusinessEmails/BusinessEmailOrderTest.php
+++ tests/unit/Orders/BusinessEmails/BusinessEmailOrderTest.php
@@ -3,35 +3,42 @@
 namespace ResellerClub\Orders\BusinessEmails\Tests;
 
 use PHPUnit\Framework\TestCase;
-use ResellerClub\Orders\BusinessEmails\BusinessEmailOrderCreate;
+use ResellerClub\Api;
+use ResellerClub\Config;
+use ResellerClub\Orders\BusinessEmails\BusinessEmailOrder;
 use ResellerClub\Orders\BusinessEmails\BusinessEmailOrderRequest;
 use ResellerClub\Orders\InvoiceOption;
 
-class BusinessEmailOrderCreateTest extends TestCase
+class BusinessEmailOrderTest extends TestCase
 {
     /**
-     * @var CreateBusinessEmailOrder
+     * @var BusinessEmailOrder
      */
-    private $business_email_order_create;
+    private $business_email_order;
 
-    public function testA()
+    public function testResponseFromBusinessEmailOrderCreate()
     {
-        $response = $this->business_email_order_create->order(
-            new BusinessEmailOrderRequest(
-                17824872,
-                'some-domain.co.in',
-                5,
-                1,
-                InvoiceOption::noInvoice()
+        $this->assertInstanceOf(
+        BusinessEmailOrderResource::class,
+            $this->business_email_order->create(
+                new BusinessEmailOrderRequest(
+                    17824872,
+                    'some-domain.co.in',
+                    5,
+                    1,
+                    InvoiceOption::noInvoice()
+                )
             )
         );
-        dd($response);
-
     }
 
     protected function setUp()
     {
         parent::setUp();
-        $this->business_email_order_create = new BusinessEmailOrderCreate();
+        $this->business_email_order = new BusinessEmailOrder(
+            new Api(
+                new Config(123, 'api_key', true)
+            )
+        );
     }
 }
