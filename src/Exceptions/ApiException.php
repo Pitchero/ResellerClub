aa7488c99706c818d764c3bfe43c9984c48af623
Robert Rosebury <r.rosebury@pitchero.com>
2017-11-10 09:05:41 +0000

    WEB-2074: Removed un-needed constructors from exception classes.

--- src/Exceptions/ApiException.php
+++ src/Exceptions/ApiException.php
@@ -7,8 +7,4 @@ use Throwable;
 
 class ApiException extends RuntimeException
 {
-    public function __construct($message = "", $code = 0, Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
 }
