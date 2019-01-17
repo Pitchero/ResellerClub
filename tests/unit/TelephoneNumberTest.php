6774c73fc8c1b5f1939e2d2d734efc566d224131
Robert Rosebury <r.rosebury@pitchero.com>
2019-01-17 14:55:31 +0000

    WEB-3078: Added in class to represent telephone numbers.

---
+++ tests/unit/TelephoneNumberTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Unit;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use ResellerClub\EmailAddress;
+use ResellerClub\TelephoneNumber;
+
+class TelephoneNumberTest extends TestCase
+{
+    public function testDiallingCode()
+    {
+        $telephoneNumber = new TelephoneNumber(
+            $diallingCode = '+44',
+            $number = '00000000'
+        );
+
+        $this->assertInternalType('string', $telephoneNumber->diallingCode());
+        $this->assertEquals('44', $telephoneNumber->diallingCode());
+
+
+        $telephoneNumber = new TelephoneNumber(
+            $diallingCode = '44',
+            $number = '00000000'
+        );
+
+        $this->assertInternalType('string', $telephoneNumber->diallingCode());
+        $this->assertEquals('44', $telephoneNumber->diallingCode());
+    }
+
+    public function testNumber()
+    {
+        $telephoneNumber = new TelephoneNumber(
+            $diallingCode = '00',
+            $number = '0113 000 0000'
+        );
+
+        $this->assertInternalType('string', $telephoneNumber->number());
+        $this->assertEquals('01130000000', $telephoneNumber->number());
+
+
+        $telephoneNumber = new TelephoneNumber(
+            $diallingCode = '00',
+            $number = '0113-000-0000'
+        );
+
+        $this->assertInternalType('string', $telephoneNumber->number());
+        $this->assertEquals('01130000000', $telephoneNumber->number());
+
+        $telephoneNumber = new TelephoneNumber(
+            $diallingCode = '00',
+            $number = '01130000000'
+        );
+
+        $this->assertInternalType('string', $telephoneNumber->number());
+        $this->assertEquals('01130000000', $telephoneNumber->number());
+
+        $telephoneNumber = new TelephoneNumber(
+            $diallingCode = '00',
+            $number = '+441130000000'
+        );
+
+        $this->assertInternalType('string', $telephoneNumber->number());
+        $this->assertEquals('441130000000', $telephoneNumber->number());
+    }
+}
